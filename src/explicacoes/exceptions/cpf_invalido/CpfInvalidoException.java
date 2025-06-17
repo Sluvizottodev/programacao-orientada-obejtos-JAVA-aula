ac4a1a1e3b4c6ab3090b640d0b522a2e9c6b1902
SLuvizotto <luvizottodev@gmail.com>
2025-06-16 22:05:24 -0300

    feat: adiciona exceção personalizada CpfInvalidoException para validação de CPF

---
+++ src/explicacoes/exceptions/cpf_invalido/CpfInvalidoException.java
@@ -0,0 +1,6 @@
+package explicacoes.exceptions.cpf_invalido;
+public class CpfInvalidoException extends RuntimeException {
+    public CpfInvalidoException(String message){
+        super(message);
+    }
+}
