8c2e98df17978b572cefbe15a0c36f66fe30fa53
SLuvizotto <luvizottodev@gmail.com>
2025-06-16 22:34:25 -0300

    feat: altera método validar na classe ValidarCpf para usar expressão regular e verifica se o CPF não é nulo

--- src/explicacoes/exceptions/cpf_invalido/ValidarCpf.java
+++ src/explicacoes/exceptions/cpf_invalido/ValidarCpf.java
@@ -1,7 +1,7 @@
 package explicacoes.exceptions.cpf_invalido;
 public class ValidarCpf{
-    public boolean validar(String cpf){
-        if (cpf.length() != 11){
+    public static boolean validar(String cpf){
+        if (!cpf.matches("\\d{11}") || cpf == null){
             throw new CpfInvalidoException("CPF deve ter 11 dígitos");
         }
         return true; 
