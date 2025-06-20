46ce298b5bc3dd7822a4bd8f993816d39f535307
SLuvizotto <luvizottodev@gmail.com>
2025-06-20 00:52:52 -0300

    feat: adiciona exceção AcaoInvalidaException para tratamento de ações inválidas

---
+++ src/questoes_extra_casa/batalha_robos/AcaoInvalidaException.java
@@ -0,0 +1,7 @@
+package questoes_extra_casa.batalha_robos;
+
+public class AcaoInvalidaException extends RuntimeException {
+    public AcaoInvalidaException(String message){
+        super(message);
+    }
+}
