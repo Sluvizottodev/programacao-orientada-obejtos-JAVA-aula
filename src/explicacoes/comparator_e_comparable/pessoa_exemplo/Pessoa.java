d127e4f47fe98c618c8ae961561cda69875fc8eb
SLuvizotto <luvizottodev@gmail.com>
2025-06-25 11:48:35 -0300

    feat: cria a classe Pessoa com atributos nome e idade para comparator

---
+++ src/explicacoes/comparator_e_comparable/pessoa_exemplo/Pessoa.java
@@ -0,0 +1,12 @@
+package explicacoes.comparator_e_comparable.pessoa_exemplo;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+}
