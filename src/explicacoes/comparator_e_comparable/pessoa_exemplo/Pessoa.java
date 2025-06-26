16bc742e92804405ec9e888d0d3648b48f3d43af
SLuvizotto <luvizottodev@gmail.com>
2025-06-26 12:56:23 -0300

    feat: implementa a interface Comparable na classe Pessoa para ordenação por nome e idade

--- src/explicacoes/comparator_e_comparable/pessoa_exemplo/Pessoa.java
+++ src/explicacoes/comparator_e_comparable/pessoa_exemplo/Pessoa.java
@@ -1,6 +1,6 @@
 package explicacoes.comparator_e_comparable.pessoa_exemplo;
 
-public class Pessoa {
+public class Pessoa implements Comparable<Pessoa> {
     private String nome;
     private int idade;
 
@@ -9,4 +9,28 @@ public class Pessoa {
         this.idade = idade;
     }
     
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public int compareTo(Pessoa outra){
+        int comparaNome = this.nome.compareTo(outra.nome);
+        if (comparaNome != 0) { // se igual a 0, seriam iguais
+            return comparaNome; 
+        }
+        // se negativo, vem antes, se positivo, vem depois
+        return Integer.compare(this.idade, outra.idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome = '" + nome + '\'' +
+                ", idade = " + idade +
+                '}';
+    }
 }
