953225faaf2e1f7865273fa9084d6d6546127a47
SLuvizotto <luvizottodev@gmail.com>
2025-06-26 15:26:59 -0300

    feat: cria a classe NotaInvalidaException para tratamento de exceções relacionadas a notas inválidas

---
+++ src/questoes_extra_casa/sistema_gestao_cursos/NotaInvalidaException.java
@@ -0,0 +1,8 @@
+package questoes_extra_casa.sistema_gestao_cursos;
+
+public class NotaInvalidaException extends RuntimeException {
+    public NotaInvalidaException(String message){
+        super(message);
+    }
+    
+}
