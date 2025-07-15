0647f97e330f8fe7a75176080365d02b1d6cfbed
SLuvizotto <luvizottodev@gmail.com>
2025-07-14 23:25:13 -0300

    feat: adiciona a classe Contato para gerenciamento de informações de contato

---
+++ src/explicacoes/padroes_de_projeto/DAO/Contato.java
@@ -0,0 +1,34 @@
+package explicacoes.padroes_de_projeto.DAO;
+
+import java.util.Calendar;
+
+public class Contato {
+    private int id;
+    private String nome, email, endereco;
+    private Calendar dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+}
