10d3fde887f49fe70e705effcc4e3cbca5791c63
sluvizottodev (notebook) <luvizottodev@gmail.com>
2025-07-20 20:44:25 -0300

    feat: adiciona método inserir na classe VeiculoAutomotorDAO para persistência de dados de veículos

--- src/questoes_provas_antigas/2025.1_p3/VeiculoAutomotorDAO.java
+++ src/questoes_provas_antigas/2025.1_p3/VeiculoAutomotorDAO.java
@@ -7,4 +7,19 @@ public class VeiculoAutomotorDAO {
     public VeiculoAutomotorDAO (Connection con){
         this.con = con;
     }
+
+    public int inserir (VeiculoAutomotor va) throws DominioException{
+        int linhasAfetadas = 0;
+        this.sql = "INSERT INTO veiculo_automotor(modelo, ano, valor_de_mercado) VALUES (?,?,?)";
+        try{
+            this.stmt = con.PreparedStatement(this.sql);
+            this.stmt.setString(1, va.getModelo());
+            this.stmt.setInt(2, va.getAno());
+            this.stmt.setDouble(3, va.getValorDeMercado());
+            linhasAfetadas = this.stmt.executeUpdate();
+        }catch(SQLException e){
+            throw new DominioException("Erro ao inserir veículo. " + e.getMessage());   
+        }
+        return linhasAfetadas;
+    }
 }
